3a77148f1c41c5a448873b1287eddce0d92c1cee
dlrmswn95 <dlrmswn95@gmail.com>
2018-09-02 12:59:08 +0900

    add sign up

---
+++ AllCle/Client/SignUp.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Client.Models;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Client
+{
+    /// <summary>
+    /// SignUp.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private async Task Save_btn_ClickAsync(object sender, RoutedEventArgs e)
+        {
+
+        }        
+    }
+}
---
+++ AllCle/Server/Models/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public interface IUserRepository
+    {
+        List<User> GetUsers();
+        void PostUsers(User _user);
+    }
+}

ff66885e1bce1c66c8d2e776f0bfd7602d2624e8
his9989 <his9989@naver.com>
2018-09-10 22:57:46 +0900

    enc and dec

--- AllCle/Server/Models/UserRepository.cs
+++ AllCle/Server/Models/UserRepository.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Server.Models
 {
@@ -28,8 +30,28 @@ namespace Server.Models
             return this.db.Query<User>(sql).ToList();
         }
 
+        public string Decrypt(string strEncrypted, string strKey)
+        {
+            TripleDESCryptoServiceProvider objDESCrypto = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider objHashMD5 = new MD5CryptoServiceProvider();
+            byte[] byteHash, byteBuff;
+            string strTempKey = strKey;
+            byteHash = objHashMD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strTempKey));
+            objHashMD5 = null;
+            objDESCrypto.Key = byteHash;
+            objDESCrypto.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Convert.FromBase64String(strEncrypted);
+            string strDecrypted = ASCIIEncoding.ASCII.GetString
+            (objDESCrypto.CreateDecryptor().TransformFinalBlock
+            (byteBuff, 0, byteBuff.Length));
+            objDESCrypto = null;
+            return strDecrypted;
+        }
+
         public void PostUsers(User _user)
         {
+            string setkey = "allcle";
+            _user.Password = Decrypt(_user.Password, setkey);
             string sql = "Insert Into Users (Id, Password) Values (@Id, @Password)";
             db.Execute(sql, _user);
         }
